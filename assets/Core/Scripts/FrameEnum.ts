bb15c52a70036f38d9a31876ba2462f0e225ca2c
Mr.Eric <451008604@qq.com>
2024-12-05 01:42:50 +0800

    feat(FrameEnum, UIMgr): Add FrameEnumTipsPosition and update showTips method

--- assets/Core/Scripts/FrameEnum.ts
+++ assets/Core/Scripts/FrameEnum.ts
@@ -2,12 +2,20 @@
  * 框架保留常量
  */
 export enum FrameEnumEventMsgID {
-    /**
-     * 语言切换
-     */
+    /**语言切换 */
     LangChange = 0x100001,
-    /**
-     * 接收到的服务器通知
-     */
+    /**接收到的服务器通知 */
     NetWorkNotify
+};
+
+/**
+ * 提示展示的位置
+ */
+export enum FrameEnumTipsPosition {
+    /**居中展示 */
+    CENTER,
+    /**上方展示 */
+    TOP,
+    /**底部展示 */
+    BOTTOM
 };
--- assets/Core/Scripts/Manager/UIMgr.ts
+++ assets/Core/Scripts/Manager/UIMgr.ts
@@ -1,5 +1,6 @@
-import { _decorator, BlockInputEvents, Canvas, color, director, Graphics, instantiate, Label, Node, Prefab, tween, UITransform, v3, Widget } from 'cc';
+import { _decorator, BlockInputEvents, Canvas, color, director, Graphics, instantiate, Label, Node, Prefab, tween, UIOpacity, UITransform, v3, Widget } from 'cc';
 import { LoadingView } from '../Components/LoadingView';
+import { FrameEnumTipsPosition } from '../FrameEnum';
 
 /**
  * 场景、弹窗管理器
@@ -139,76 +140,60 @@ export class UIMgr {
     }
 
 
-    showTips(text: string = '', { gravity = 'CENTER', duration = 1, bg_color = color(102, 102, 102, 200) } = {}) {
-        // canvas
+    /**
+     * 展示提示内容
+     * @param text 提示的文字
+     * @param param1 可选配置项
+     */
+    showTips(text: string = '', { gravity = FrameEnumTipsPosition.CENTER, duration = 1, bg_color = color(102, 102, 102, 255) } = {}) {
         let canvas = this.tipsLayer || director.getScene().getComponentInChildren(Canvas).node;
-        let transform = canvas.getComponent(UITransform);
-        app.log.info(transform.contentSize, transform.height);
-
-        let width = transform.width;
-        let height = transform.height;
-
-        // 节点
-        let bgNode = new Node();
-
+        let canvasTransform = canvas.getComponent(UITransform);
+        let width = canvasTransform.width;
+        let height = canvasTransform.height;
 
         // Lable文本格式设置
         let textNode = new Node();
         let textLabel = textNode.addComponent(Label);
+        let textTransform = textNode.addComponent(UITransform);
         textLabel.horizontalAlign = Label.HorizontalAlign.CENTER;
         textLabel.verticalAlign = Label.VerticalAlign.CENTER;
         textLabel.fontSize = 30;
         textLabel.string = text;
 
-        transform = textNode.addComponent(UITransform);
-
         // 当文本宽度过长时，设置为自动换行格式
         if (text.length * textLabel.fontSize > (width * 3) / 5) {
-            // transform.width = (width * 3) / 5;
             textLabel.overflow = Label.Overflow.RESIZE_HEIGHT;
         } else {
-            transform.width = text.length * textLabel.fontSize;
+            textTransform.width = text.length * textLabel.fontSize;
         }
         let lineCount = ~~((text.length * textLabel.fontSize) / ((width * 3) / 5)) + 1;
-        transform.height = textLabel.fontSize * lineCount;
+        textTransform.height = textLabel.fontSize * lineCount;
 
         // 背景设置
+        let bgNode = new Node();
         let ctx = bgNode.addComponent(Graphics);
-        ctx.arc(
-            -transform.width / 2,
-            0,
-            transform.height / 2 + 20,
-            0.5 * Math.PI,
-            1.5 * Math.PI,
-            true
-        );
-        ctx.lineTo(transform.width / 2, -(transform.height / 2 + 20));
-        ctx.arc(
-            transform.width / 2,
-            0,
-            transform.height / 2 + 20,
-            1.5 * Math.PI,
-            0.5 * Math.PI,
-            true
-        );
-        ctx.lineTo(-transform.width / 2, transform.height / 2 + 20);
+        let bgNodeOpacity = bgNode.addComponent(UIOpacity);
+        ctx.arc(-textTransform.width / 2, 0, textTransform.height / 2 + 20, 0.5 * Math.PI, 1.5 * Math.PI, true);
+        ctx.lineTo(textTransform.width / 2, -(textTransform.height / 2 + 20));
+        ctx.arc(textTransform.width / 2, 0, textTransform.height / 2 + 20, 1.5 * Math.PI, 0.5 * Math.PI, true);
+        ctx.lineTo(-textTransform.width / 2, textTransform.height / 2 + 20);
         ctx.fillColor = bg_color;
         ctx.fill();
 
-        bgNode.addChild(textNode);
-
-        // gravity 设置Toast显示的位置
-        if (gravity === "CENTER") {
+        // gravity 设置tips显示的位置
+        if (gravity === FrameEnumTipsPosition.CENTER) {
             bgNode.setPosition(v3(0, 0, 0));
-        } else if (gravity === "TOP") {
+        } else if (gravity === FrameEnumTipsPosition.TOP) {
             bgNode.setPosition(bgNode.getPosition().add(v3(0, (height / 5) * 2, 0)));
-        } else if (gravity === "BOTTOM") {
+        } else if (gravity === FrameEnumTipsPosition.BOTTOM) {
             bgNode.setPosition(bgNode.getPosition().subtract(v3(0, (height / 5) * 2, 0)));
         }
 
+        bgNode.addChild(textNode);
         canvas.addChild(bgNode);
         // 执行动画
-        tween(bgNode).to(duration, { position: v3(0, 0, 0) }, { easing: "fade" }).destroySelf().start();
+        tween(bgNodeOpacity).delay(duration).to(.5, { opacity: 0 }).start();
+        tween(bgNode).delay(duration).by(.5, { position: v3(0, 100, 0) }).destroySelf().start();
     }
 
 }
